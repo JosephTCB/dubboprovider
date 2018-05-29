3327ace1163d1aa8061b61f0c74a658e24c8c242
Joseph <1160156324@qq.com>
2018-05-29 14:13:26 +0800

    add zookeeper

--- src/main/java/com/example/demo/dubbo/HelloServiceImpl.java
+++ src/main/java/com/example/demo/dubbo/HelloServiceImpl.java
@@ -1,14 +1,15 @@
-package com.example.demo.service;
+package com.example.demo.dubbo;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.example.demo.service.HelloService;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by lichujun on 2018-05-28.
  */
-@Service(interfaceClass = IHelloService.class)
+@Service
 @Component
-public class HelloServiceImpl implements IHelloService {
+public class HelloServiceImpl implements HelloService {
     public String hello(){
         return "hello world";
     }
--- src/main/java/com/example/demo/service/HelloService.java
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -3,6 +3,6 @@ package com.example.demo.service;
 /**
  * Created by lichujun on 2018-05-28.
  */
-public interface IHelloService {
+public interface HelloService {
     public String hello();
 }
